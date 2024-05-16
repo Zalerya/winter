bd9c70b5d82faa6775335c564a08db84daa2b7a1
Zalerya <153430442+Zalerya@users.noreply.github.com>
2024-05-16 20:07:10 +0300

    Lister controller
    - Annotation controller
    - Mise a jour defaultController (+ attribut boolean checked + list<Controller>)

--- src/main/java/com/itu/prom16/controller/DefaultController.java
+++ src/main/java/com/itu/prom16/controller/DefaultController.java
@@ -1,13 +1,20 @@
 package com.itu.prom16.controller;
 
 import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.itu.prom16.annotation.Controller;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "DefaultController", value = "/")
 public class DefaultController extends HttpServlet {
     private String message;
+    boolean checked = false ;
+    List<String> nomController = new ArrayList<>();
 
     public void init() {
         message = "Hello World!";
@@ -29,5 +36,43 @@ public class DefaultController extends HttpServlet {
     }
 
     public void destroy() {
+    }
+
+    protected void processRequest(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.setContentType("text/plain");
+        PrintWriter out = res.getWriter();
+
+        if (!checked) {
+            try {
+                String controllerPackage = getServletConfig().getInitParameter("controllerChecker");
+                ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+                URL url = classLoader.getResource(controllerPackage);
+                if (url != null) {
+                    File directory = new File(url.getFile().replace("%20", " "));
+                    if (directory.exists() && directory.isDirectory()) {
+                        File[] files = directory.listFiles();
+                        if (files != null) {
+                            for (File file : files) {
+                                if (file.isFile() && file.getName().endsWith(".class")) {
+                                    String className = file.getName().substring(0, file.getName().lastIndexOf('.'));
+                                    Class<?> clazz = Class.forName(String.format("%s.%s", controllerPackage, className));
+                                    if (clazz.isAnnotationPresent(Controller.class)) {
+                                        nomController.add(clazz.getName());
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                checked = true;
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        if (checked){
+            for (int i = 0; i < nomController.size(); i++) {
+                out.println(nomController.get(i));
+            }
+        }
     }
 }
