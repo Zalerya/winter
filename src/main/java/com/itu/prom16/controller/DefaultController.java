bdd46d0a80b30b6650d96cdf8508a24aca5d702d
Zalerya <153430442+Zalerya@users.noreply.github.com>
2024-06-07 11:42:46 +0300

    - Gestion des erreurs (url, retour)
    - Ajout d'erreur dans ModelView (doublon url)
    - Ajout d'erreur dans DefaultController (url inexistant, type de retour ModelView invalide)

--- src/main/java/com/itu/prom16/controller/DefaultController.java
+++ src/main/java/com/itu/prom16/controller/DefaultController.java
@@ -22,7 +22,7 @@ import jakarta.servlet.annotation.*;
 public class DefaultController extends HttpServlet {
     HashMap<String, ClassMethod> mesController = new HashMap();
 
-    public void peuplerList (String controllerPackage) throws ClassNotFoundException {
+    public void peuplerList (String controllerPackage) throws Exception {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         String dir = controllerPackage.replace(".", "/");
         URL url = classLoader.getResource(dir);
@@ -31,8 +31,7 @@ public class DefaultController extends HttpServlet {
             //System.out.println(directory.toString());
             if (directory.exists() && directory.isDirectory()) {
                 File[] files = directory.listFiles();
-                //System.out.println(files.length);
-                if (files != null) {
+                if (files != null && files.length > 0) {
                     for (File file : files) {
                         if (file.isFile() && file.getName().endsWith(".class")) {
                             String className = file.getName().substring(0, file.getName().lastIndexOf('.'));
@@ -42,12 +41,20 @@ public class DefaultController extends HttpServlet {
                                 for (Method methode : lesMethodes) {
                                     if (methode.isAnnotationPresent(Get.class)) {
                                         Get get = methode.getAnnotation(Get.class);
-                                        mesController.put(get.path(), new ClassMethod(clazz.getName(), methode.getName()));
+                                        if (mesController.containsKey(get.path())) {
+                                            throw new Exception("Le chemin '" + get.path() + "' existe en double.");
+                                        }
+                                        else {
+                                            mesController.put(get.path(), new ClassMethod(clazz.getName(), methode.getName()));
+                                        }
                                     }
                                 }
                             }
                         }
                     }
+                }
+                else {
+                    throw new Exception("Le dossier est vide.");
                 }
             }
         }
@@ -65,26 +72,17 @@ public class DefaultController extends HttpServlet {
         try {
             String controllerPackage = getServletConfig().getInitParameter("controllerChecker");
             this.peuplerList(controllerPackage);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         //response.setContentType("text/html");
         try {
             processRequest(request, response);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (InvocationTargetException e) {
-            throw new RuntimeException(e);
-        } catch (NoSuchMethodException e) {
-            throw new RuntimeException(e);
-        } catch (InstantiationException e) {
-            throw new RuntimeException(e);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
+        } catch (Exception e) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, e.getMessage());
         }
     }
 
@@ -95,15 +93,21 @@ public class DefaultController extends HttpServlet {
     public void destroy() {
     }
 
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
         // Hello
         PrintWriter out = response.getWriter();
-        out.println("<html><body>");
 
         String urlDemande = request.getRequestURI().replace(request.getContextPath(), "");
-        out.println("<h1>" + urlDemande + "</h1>");
-        if (mesController.containsKey(urlDemande)) {
+
+        if (urlDemande.equalsIgnoreCase("/")) {
+            out.println("<html><body>");
+            out.println("<h1>" + urlDemande + "</h1>");
+            out.println("<p>" + "Page d'acceuil" + "</p>");
+            out.println("</body></html>");
+        }
+
+        else if (mesController.containsKey(urlDemande)) {
             ClassMethod classMethod = mesController.get(urlDemande);
 
             if (this.invokeMethode(classMethod).getClass() == ModelView.class) {
@@ -112,17 +116,18 @@ public class DefaultController extends HttpServlet {
                 request.getRequestDispatcher(modelView.getUrl()).forward(request, response);
             } else if (this.invokeMethode(classMethod).getClass() == String.class) {
                 String result = (String) this.invokeMethode(classMethod);
-                out.println("<p>" + "TROUVE" + "</p>");
+                out.println("<html><body>");
+                out.println("<h1>" + urlDemande + "</h1>");
                 out.println("<p>" + "RESULT" + result + "</p>");
-                //out.println("<p>" + classMethod.getClazz() + "</p>");
-                //out.println("<p>" + classMethod.getMethod() + "</p>");
+                out.println("</body></html>");
+            }
+            else {
+                throw new Exception ("Le type de retour n'est pas pris en charge. (String et ModelView uniquement)");
             }
         }
 
         else {
-            out.println("<p>" + "INEXISTANT" + "</p>");
+            throw new Exception("L'URL est introuvable.");
         }
-
-        out.println("</body></html>");
     }
 }
