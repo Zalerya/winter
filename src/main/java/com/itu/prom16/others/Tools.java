79ad504feed15142d08b3b51ddaf40d76efb23af
Zalerya <153430442+Zalerya@users.noreply.github.com>
2024-06-21 20:35:31 +0300

    - Sprint 7
    - Rajout d'une partie pour traiter des Object de classe en parametre
    - Ajout d'une annotation input pour les attributs des objets

--- src/main/java/com/itu/prom16/others/Tools.java
+++ src/main/java/com/itu/prom16/others/Tools.java
@@ -1,4 +1,9 @@
 package com.itu.prom16.others;
 
 public class Tools {
+    public static boolean isCustomClass (Class clazz) {
+        // Le chargeur de classes pour les bibliothèques standards de Java est null (Bootstrap ClassLoader)
+        ClassLoader classLoader = clazz.getClassLoader();
+        return classLoader != null;
+    }
 }
